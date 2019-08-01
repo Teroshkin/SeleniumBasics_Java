b5e0cb6eda93197be855b41f42eb1e3d173394a5
Tigran Gegamyan <tigran.gegamyan@dataart.com>
2019-08-01 13:11:39 +0300

    for GitHub

---
+++ src/documentation/IntelijShortcuts.java
@@ -0,0 +1,8 @@
+package documentation;
+
+public class IntelijShortcuts {
+}
+
+/*
+    1. cmd + d - копирование предыдущей строки
+ */
---
+++ src/documentation/TerminalShortcutsCommands.java
@@ -0,0 +1,12 @@
+package documentation;
+
+public class TerminalShortcutsCommands {
+    /*
+    Ctrl + A - перейти к началу строки
+    Сtrl + E - перейти к концу строки
+    Alt + <> - перемещение по слову
+    ctrl + K -  чтобы убить строку, начинающуюся с позиции курсора
+    defaults write com.apple.finder AppleShowAllFiles TRUE ; killall Finder - показать все скрытые файлы
+    defaults write com.apple.finder AppleShowAllFiles FALSE ; killall Finder - обратить вскрытие файлов
+     */
+}
