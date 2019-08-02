bdba8eebd1d1a8b9afc90ca033e71c325c073f95
Tigran Gegamyan <tigran.gegamyan@dataart.com>
2019-08-02 14:59:47 +0300

    for GitHub

--- src/documentation/GitHubAuth.java
+++ src/documentation/GitHubAuth.java
@@ -1,9 +1,5 @@
-package documentation;
 
-public class GitHubAuth {
 
-}
-    /*
     github recently switched to an https scheme as the default for cloning repos. as a side effect you may suddenly be prompted for a 'Username' and 'Password' when you push where, previously, you were able to do so without typing in credentials. the solution is to cause git to cache https credentials which is easy, since git uses curl under the covers
 
 in your home directory create a file called '.netrc', for example
@@ -17,4 +13,3 @@ login YOUR_GITHUB_USERNAME
 password YOUR_GITHUB_PASSWORD
 
 fixed!
-     */
