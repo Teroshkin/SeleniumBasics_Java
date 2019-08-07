95960f367948b126fc8fec7a9a82c9f2cec65012
Tigran Gegamyan <tigran.gegamyan@dataart.com>
2019-08-07 16:24:24 +0300

    some updates

---
+++ src/Java/Dynamic_Programme.java
@@ -0,0 +1,22 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Dynamic_Programme {
+
+        public static void main (String[]args){
+        System.out.println("Hello");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+    }
+    }
+
+
+// comment
+//class - contains everything
+//methods - do something
+// arguments - what you pass to a method (part of the calling)
+//parameters
+//statements - telling the computer something
+//properties - store something`
