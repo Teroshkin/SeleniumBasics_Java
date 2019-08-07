467fe15942b0d387229fe00751711a8e73b74b81
Tigran Gegamyan <tigran.gegamyan@dataart.com>
2019-08-07 18:14:23 +0300

    some updates

---
+++ src/Java/HelloWorldPart2.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class HelloWorldPart2 {
+    public static void second (String[] args) {
+
+        System.out.println("Hello World!");
+
+        //выводим количество аргументов заданных в командной строке
+        System.out.println("Всего аргументов в командной строке: "+args.length);
+
+    }
+}
