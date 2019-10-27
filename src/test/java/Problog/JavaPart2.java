c4e549ed9d070de0d92b398e9602dc3f36daef26
PUKOT <ishogashii@gmail.com>
2019-10-27 22:02:33 +0200

    dasdasda

---
+++ src/test/java/Problog/JavaPart2.java
@@ -0,0 +1,12 @@
+package Problog;
+
+public class JavaPart2 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+        //выводим количество аргументов заданных в командной строке
+        System.out.println("Всего аргументов в командной строке: "+args.length);
+
+    }
+}
